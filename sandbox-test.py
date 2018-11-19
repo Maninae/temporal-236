2b2ecdec7aa1a14517d9076d532063cf49cc7ca1
Owen Wang <ojwang@stanford.edu>
2018-11-18 21:53:54 -0800

    Testing AnimatedDataset in sandbox

--- sandbox-test.py
+++ sandbox-test.py
@@ -1,7 +1,8 @@
-from util.data_loader import BreakoutDataset
-
+from util.data_loader import AnimatedDataset
+from util.paths import sequences_dir
+from os.path import join
 
 if __name__ == "__main__":
-    ds = BreakoutDataset()
+    ds = AnimatedDataset(join(sequences_dir, "animated"))
     print(len(ds))
     print(ds[247][0][0].shape)
