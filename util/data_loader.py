9f02d3a24d4d44b2869b0f779bc89600a85cc46c
Cody Keola Kala <codyakala@stanford.edu>
2018-11-28 12:40:25 -0800

    cleaned up import statements and made minor change to GenericDataset docstring

--- util/data_loader.py
+++ util/data_loader.py
@@ -1,61 +1,62 @@
-import random
+# Standard imports
 import os
-from os.path import join, isdir, isfile, basename
-
+import random
 import torch
-from torch.utils.data import Dataset, DataLoader
-from util.paths import sequences_dir
-
+from os.path import basename
+from os.path import isdir
+from os.path import isfile
+from os.path import join
 from PIL import Image
 from torchvision import transforms
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+# Custom imports
+from util.paths import sequences_dir
 
 class GenericDataset(Dataset):
-    """
-        We want to return x=(frame_k, frame_k+2), y=(frame_k+1).
-    """
+    """ Returns frame examples in the following format:
 
+            x[i] = (frame_i, frame_i+2)
+            y[i] = frame_i+1
+
+    """
     _default_transforms = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.5,), (0.5,))])  # mean and stddev
 
     def __init__(self, directory, transforms=None):
         super(GenericDataset, self).__init__()
-
-        # Path is relative to repo root
-
-        # TODO: Change back to "all-framnes" or migrate completely to "all-frames{"
-
         self.directory = directory
-        # self.directory = join(sequences_dir, "breakout", "all-frames")
-        self.files = sorted([filename for filename in os.listdir(self.directory) if filename.endswith(".png")])
-        self.transforms = transforms if transforms is not None else BreakoutDataset._default_transforms
+        self.files = sorted([filename for filename in os.listdir(directory) if filename.endswith(".png")])
+        self.transforms = transforms if transforms else GenericDataset._default_transforms
 
     def __len__(self):
-        """ Provides the size of the dataset.
-        """
+        """ Returns the size of the dataset. """
         return len(self.files) - 2
+
+    def __getitem__(self, i):
+        """ Supports integer indexing from 0 to len(self) exclusive. """
+        before, current, after = map(self._tensor_from_img_file, self.files[i:i + 3])
+        x = (before, after)
+        y = current
+        return (x, y)
 
     def _tensor_from_img_file(self, filename):
         filepath = join(self.directory, filename)
         image = Image.open(filepath).convert("L")
         return self.transforms(image)
 
-    def __getitem__(self, i):
-        """ Supports integer indexing from 0 to len(self) exclusive.
-        """
-        before, current, after = map(self._tensor_from_img_file, self.files[i:i + 3])
-
-        x = (before, after)
-        y = current
-        return (x, y)
 
 class OceanDataset(GenericDataset):
+    """ Serves examples from the `ocean.mp4` video. """
     def __init__(self):
         self.directory = join(sequences_dir, "ocean", "all-frames")
         super(OceanDataset, self).__init__(self.directory)
 
 
 class BreakoutDataset(GenericDataset):
+    """ Serves examples from the Breakout video. """
     def __init__(self):
         self.directory = join(sequences_dir, "breakout", "all-frames")
         super(BreakoutDataset, self).__init__(self.directory)
