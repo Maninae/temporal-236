ba55fd7dfea7d5e0b0a8dc7ffdb8fbc5c51d086f
Cody Keola Kala <codyakala@stanford.edu>
2018-12-12 13:03:48 -0800

    added code to delete tensor references to improve GPU memory cache usage

--- run.py
+++ run.py
@@ -92,9 +92,6 @@ dataloader = DataLoader(dataset, batch_size=config["batch_size"], shuffle=True)
 #  Training
 # ----------
 
-# TODO: Add plots for discriminator and generator losses
-# TODO: Split datasets into train and val
-# TODO: Modify train loop to also evaluate on val
 # TODO: Update code to conform to most recent PyTorch version
 
 # Track losses for each batch
@@ -180,6 +177,15 @@ for epoch in range(config["n_epochs"]):
             plt.title("training loss for generator")
             plt.savefig("plots/{}_train/g_{}.png".format(config["dataset"], batches_done))
 
+        # Delete all references (so they can be released)
+        del inputs
+        del real_imgs
+        del fake_imgs
+        del g_loss_train
+        del real_loss
+        del fake_loss
+        del d_loss_train
+        torch.cuda.empty_cache() 
 
     # ------------
     #  VALIDATION
@@ -247,7 +253,17 @@ for epoch in range(config["n_epochs"]):
             plt.ylabel("loss")
             plt.title("validation loss for generator")
             plt.savefig("plots/{}_val/g_{}.png".format(config["dataset"], batches_done))
+            plt.clf()
 
+        # Delete variable references so GPU memory cache can be released
+        del inputs
+        del real_imgs
+        del fake_imgs
+        del g_loss_val
+        del real_loss
+        del fake_loss
+        del d_loss_val
+        torch.cuda.empty_cache()
 
     # Save the state of the model
     torch.save((generator.state_dict(),
@@ -255,69 +271,3 @@ for epoch in range(config["n_epochs"]):
                optimizer_G.state_dict(),
                optimizer_D.state_dict()),
                "checkpoints/{}/checkpoint_{}.pth".format(config["dataset"], epoch))
-
-"""
-for epoch in range(config["n_epochs"]):
-    for i, (x, y) in enumerate(dataloader):
-
-        # -------
-        #  Setup
-        # -------
-
-        # Inputs and outputs
-        inputs = Variable(torch.cat(x, 1).type(Tensor))
-        real_imgs = Variable(y.type(Tensor))
-
-        # Adversarial ground truths
-        valid = Variable(Tensor(y.shape[0], 1).fill_(1.0), requires_grad=False)
-        fake = Variable(Tensor(y.shape[0], 1).fill_(0.0), requires_grad=False)
-
-        # -----------------
-        #  Train Generator
-        # -----------------
-
-        # Loss measures generator's ability to fool the discriminator
-        gen_imgs = generator(inputs)
-        g_loss = adversarial_loss(discriminator(gen_imgs), valid)
-        optimizer_G.zero_grad()
-        g_loss.backward()
-        optimizer_G.step()
-
-        # ---------------------
-        #  Train Discriminator
-        # ---------------------
-
-        # Loss measures discriminator's ability classify real from generated samples
-        real_loss = adversarial_loss(discriminator(real_imgs), valid)
-        fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
-        d_loss = (real_loss + fake_loss) / 2 
-        optimizer_D.zero_grad()
-        d_loss.backward()
-        optimizer_D.step()
-
-        # Print progress
-        print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % 
-              (epoch, config["n_epochs"], i, len(dataloader), d_loss.item(), 
-              g_loss.item()))
-
-        # Store losses for plotting
-        d_losses.append(d_loss.item())
-        g_losses.append(g_loss.item())
-
-        # Generate sample output
-        batches_done = epoch * len(dataloader) + i
-        if batches_done % config["sample_interval"] == 0:
-            save_image(gen_imgs.data[:25], 
-                       "samples/{}/{}.png".format(config["dataset"], batches_done),
-                       nrow=5, normalize=True)
-            # TODO: Add code to plot losses (and other metrics) here
-
-        
-
-    # Save the state of the model
-    torch.save((generator.state_dict(),
-                discriminator.state_dict(),
-                optimizer_G.state_dict(),
-                optimizer_D.state_dict()), 
-                "checkpoints/{}/checkpoint_{}.pth".format(config["dataset"], epoch))
-"""
